eadff2ad15d3e1c2611effd0057010055d9b12f8
juanfer2 <jvilladiego@ayenda.co>
2021-12-30 07:46:34 -0500

    Config store

---
+++ src/store/reducers/healt_reducer.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const healtSlice = createSlice({
+  name: 'healt',
+  initialState: {status: 'PENDING...'},
+  reducers: { 
+    updateStatus: (state) => { state.status = 'OK' }
+  }
+});
+
+export const { updateStatus } = healtSlice.actions
+export default healtSlice.reducer
