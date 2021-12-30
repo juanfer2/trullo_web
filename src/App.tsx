9fdbadc4a922e3c70ead50e075c744ecbf22a449
juanfer2 <64051193+juanfer2@users.noreply.github.com>
2021-12-29 22:43:59 -0500

    Config absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from '@components/card';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Card/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/card/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      Hello Card
+    </div>
+  )
+}
+
+export default Card;
