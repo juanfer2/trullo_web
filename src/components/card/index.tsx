b9527edc64fa1b2478122d5c80a0cdf354cf364d
juanfer2 <jvilladiego@ayenda.co>
2021-12-31 16:58:54 -0500

    Create components baged, card and image

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import {CardStyle} from '@components/card/card_styled'
+import Image from '@components/image'
+import Baged from '@components/baged';
 
-function Card() {
-  return <div>Hello Card</div>;
+interface CardInterface {
+  title: string;
+  listBadged: any[];
+  imageUrl: string;
+  avatarUrls: string[];
+}
+
+function Card({title, listBadged, imageUrl, avatarUrls}: CardInterface) {
+  return (
+    <CardStyle>
+      { 
+        imageUrl && <Image imageUrl={imageUrl} type='default'/>
+      }
+      <h4 className='title'>{title}</h4>
+      {
+        listBadged && 
+        <div  className='baged-list'>
+          {listBadged.map((baged: any) =>
+          <Baged key={baged.label} type={baged.type} label={baged.label}/>
+        )}
+        </div>
+       
+      }
+      <div className='info'>
+        <div className='avatars'>
+          {avatarUrls && avatarUrls.map((image: any) => 
+          <Image key={image} imageUrl={image} type='small'/>)}
+        </div>
+        <div className="quantity">
+          ...
+        </div>
+      </div>
+    </CardStyle>
+  );
 }
 
 export default Card;
